e6058c88b8ea3439501ca3e7f97f2ab6882a1725
David Eisner <david.eisner@oriel.oxon.org>
2018-12-28 14:40:10 +0000

    Sound some alternative phoneme spellings

--- renderer.js
+++ renderer.js
@@ -17,6 +17,34 @@ let r_word = /tmcore_assets_phonics_words_audio_(.*)_ogg.ogg/;
 let phonemes = window.phonemes = {};
 let words = window.words = {}
 let synth = window.synth = {}
+let alternatives = {
+  'n': ["kn", "gn"],
+  'm': ["mb", "mn"],
+  'd': ["ed"],
+  'g': ["\\bgh"],
+  'l': ["ll"],
+  'f': ["ff", "ph", "\\Bgh"],
+  'j': ["\\bg(?=[eiy])", "dge", "\\Bge"],
+  'oa': ["o\\we", "ow", "\\Bo", "oe", "ough", "eau", "ou"],
+  'ee': ["e\\we", "y", "ea", "ie", "ey", "\\Be$", "\\Bae$"],
+  'ai': ["a\\we", "ay", "ei", "eigh", "ey", "ae", "ea"],
+  'ie': ["i\\we", "igh", "\\By$", "\\bei", "\\bai", "\\Bye", "uy"],
+  'or': ["al", "au", "aw", "ore", "ough", "ar", "oar", "our", "oor"],
+  'z': ["\\Bs$", "\\bse$", "ze"],
+  'v': ["ve"],
+  'oo': ["ue", "u\\we", "ew", "\\bu$", "\\bo$", "ui", "ou", "ough", "eu"],
+  'w': ["wh", "ui"],
+  'ch': ["tch"],
+  'sh': ["sch", "\\Bche$", "\\Bci", "\\Bti", "\\Bsi"],
+  'ou': ["ow", "ough"],
+  'oi': ["oy", "uoy"],
+  'ue': ["u\\we", "ew", "\\bu", "eu"],
+  'er': ["ir", "ur", "ear", "or", "our", "re"],
+  'ar': ['ear', 'al', '\\bau', 'er'],
+  'air': ['are', 'ear', 'ere', 'aer'],
+  'zh': ["\\Bsi", "\\Bs\\B", "\\Bg\\B", "\\Bge$"],
+  'ool': ["\\Ble", "\\Bal$", "\\Bil$", "\\Bol$", "\\Bul"]
+};
 let res = path.join(path.dirname(process.mainModule.filename), "res", "raw");
 _.each(fs.readdirSync(res), f => {
   let sound = () => new Howl({
@@ -24,7 +52,16 @@ _.each(fs.readdirSync(res), f => {
   });
   var match = r_phoneme.exec(f);
   if (match) {
-    phonemes[match[1]] = sound();
+    let s = sound();
+    _.each((alternatives[match[1]] || []).concat([match[1]]), phoneme => {
+      if (!phonemes[phoneme] || phoneme == match[1] || alternatives[phoneme]) {
+        phonemes[phoneme] = {
+          r: new RegExp("^" + phoneme + "$"),
+          spelling: phoneme.replace(/[.]|\\w/g, "_").replace(/\\.|\(.*\)|\[.*\]|\W/g, ""),
+          sound: s
+        };
+      }
+    });
     return;
   }
   match = r_word.exec(f);
@@ -34,6 +71,10 @@ _.each(fs.readdirSync(res), f => {
   }
 });
 let r_phonemes = new RegExp("(" + _.keys(phonemes).join("|") + ")$");
+_.each(phonemes, p => {
+  if (!phonemes[p.spelling])
+    phonemes[p.spelling] = p;
+});
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -66,7 +107,7 @@ let feedproc = async () => {
     display.text(x);
     var sound;
     if (sound = phonemes[x])
-      play(sound, feedproc);
+      play(sound.sound, feedproc);
     else if (sound = words[x])
       play(sound, feedproc);
     else {
@@ -104,9 +145,15 @@ $('[contenteditable]')
     //console.log(e);
     if (/\w/.test(String.fromCharCode(e.keyCode))) {
       let m = r_phonemes.exec($(e.target).text() + e.key);
-      if (m)
-        feed(m[1]);
-      else
+      if (m) {
+        var phoneme = phonemes[m[1]];
+        if (!phoneme)
+          phoneme = _.find(phonemes, p => p.r.test(m[1]));
+        if (phoneme)
+          feed(phoneme.spelling);
+        else
+          feed(m[1]);
+      } else
         feed(e.key);
     } else if (/^\s+$/.test(String.fromCharCode(e.keyCode))) {
       let txt = $(e.target).text();
